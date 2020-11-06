40da32f9c5002e7306951d03c88d737a715986ae
leogtzr <leogutierrezramirez@gmail.com>
2020-11-05 17:25:54 -0700

    sorting by itemgetter

---
+++ data_structures_and_algorithms/sorting_a_list_of_dictionaries_by_a_common_key1.py
@@ -0,0 +1,22 @@
+from operator import itemgetter
+
+rows = [
+    {'fname': 'Brian', 'lname': 'Jones', 'uid': 1003},
+    {'fname': 'David', 'lname': 'Beazley', 'uid': 1002},
+    {'fname': 'John', 'lname': 'Cleese', 'uid': 1001},
+    {'fname': 'Big', 'lname': 'Jones', 'uid': 1004}
+]
+
+# print(rows)
+
+rows_by_fname = sorted(rows, key=itemgetter('fname'))
+
+print(rows_by_fname)
+
+rows_by_uid = sorted(rows, key=itemgetter('uid'))
+print(rows_by_uid)
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+rows_by_lfname = sorted(rows, key=itemgetter('lname','fname'))
+print(rows_by_lfname)
