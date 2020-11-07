080c024fcf1d0b51fcfd3fc8f63b6ecb53426871
leogtzr <leogutierrezramirez@gmail.com>
2020-11-07 14:43:54 -0700

    creating dictionaries 1

---
+++ data_structures_and_algorithms/dictionary_comprehension1.py
@@ -0,0 +1,28 @@
+prices = {
+   'ACME': 45.23,
+   'AAPL': 612.78,
+   'IBM': 205.55,
+   'HPQ': 37.20,
+   'FB': 10.75
+}
+
+# Make a dictionary of all prices over 200
+prices_over_200 = {
+	key:value for key, value in prices.items() if value > 200
+}
+
+# print(prices_over_200)
+
+# Make a dictionary of tech stocks
+tech_names = { 'AAPL', 'IBM', 'HPQ', 'MSFT' }
+
+tech_stocks = {
+	key:value for key, value in prices.items() if key in tech_names
+}
+
+print(prices_over_200)
+print(tech_stocks)
+
+p1 = dict((key, value) for key, value in prices.items() if value > 200)
+
+print(p1)
