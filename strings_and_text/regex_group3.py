fc41c94163310e9a45c28279fe99f352cb1b0a6d
leogtzr <leogutierrezramirez@gmail.com>
2020-11-13 18:05:15 -0700

    getting multiple tuples

---
+++ strings_and_text/regex_group3.py
@@ -0,0 +1,9 @@
+import re
+
+datepat = re.compile(r'(\d+)/(\d+)/(\d+)')
+
+text = 'Today is 11/27/2012. PyCon starts 3/13/2013.'
+
+for month, day, year in datepat.findall(text):          # Destructuring.
+    # print(len(oc))
+    print(f"{month}-{day}-{year}")
